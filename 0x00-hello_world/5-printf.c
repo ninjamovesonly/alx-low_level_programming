33df8a97b4d32d8f9d4c526a09d4a4d6748ec774
nedu <chineduabalogu@gmail.com>
2022-12-08 15:07:20 -0800

    bettys currently my best friend lol

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main program block
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
