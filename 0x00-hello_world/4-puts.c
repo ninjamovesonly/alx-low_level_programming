947ad8ed7e32979831c5847e0e36d6bb0827adbf
nedu <chineduabalogu@gmail.com>
2022-12-08 14:34:24 -0800

    betty never satisfied but lets see

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ *  main - main program block
+ *  return: 0
+ */
+
 int main(void)
+
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
