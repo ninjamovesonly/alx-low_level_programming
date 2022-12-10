325e38acc6be13ecf94e956beb0180535ef795af
nedu <chineduabalogu@gmail.com>
2022-12-09 18:31:38 -0800

    confirm check code till task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ * Description: patience, persistence etc
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for(c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar("\n");
+	return (0);
+}
