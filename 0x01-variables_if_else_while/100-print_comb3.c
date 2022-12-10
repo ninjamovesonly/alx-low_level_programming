22895f5a712c705394f39125ffc63429201d029f
nedu <chineduabalogu@gmail.com>
2022-12-09 20:12:37 -0800

    wrap up

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ * Description: combination
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = 0; d1 < 9; d1++)
+	{
+		putchar((d1 % 10) + '0');
+		putchar((d2 % 10) + '0');
+
+		if (d1 == 8 && d2 == 9)
+		{
+			continue;
+		}
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
