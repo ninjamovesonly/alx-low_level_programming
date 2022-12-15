25e2ee305cf8bcef32ac6506ef72e815cf280727
nedu <chineduabalogu@gmail.com>
2022-12-14 16:28:13 -0800

    check code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Description: testing betty
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/**
+* Description: betty still not satisfied
+* while (i < 10)
+* {
+*      putchar(i);
+* }
+*/
+/* Description - betty keeps asking */
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
