229c64f1474440209db665a49db121e463c4a3d3
nedu <chineduabalogu@gmail.com>
2022-12-15 18:27:29 -0800

    check again

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Description: testing betty
+ * main - cause an infinite loop
  * Return: 0
  */
 
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-i = 0;
-/**
-* Description: betty still not satisfied
-* while (i < 10)
-* {
-*      putchar(i);
-* }
-*/
-/* Description - betty keeps asking */
-printf("Infinite loop avoided! \\o/\n");
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
