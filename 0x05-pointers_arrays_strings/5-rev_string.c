fbb487aed8e50524609046b5e27493a47d39f363
Prince <38994284+ninjamovesonly@users.noreply.github.com>
2022-12-21 01:19:21 +0100

    tweaks and test

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,11 @@ void rev_string(char *s)
 	while (s[i] != '\0')
 	    i++;
 
-	int (m, h) = ((i - 1), (m / 2));
+	int m, h = i - 1, m / 2;
 
 	while (h >= 0)
 	{
-		char (f, l) = (s[m - h], s[h]);
+		char f, l = s[m - h], s[h];
 		s[h] = f;
 		s[m - h] = l;
 		h--;
